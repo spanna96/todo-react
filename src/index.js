bf926b194fbf3029c8b194c137563cac56a723e6
Anna <spanna.exceedteam@gmail.com>
2019-11-25 13:01:03 +0300

    added style

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoApp from './classes/TodoApp';
+/* import './index.css'; */
+
+
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('todos-example')
+);
